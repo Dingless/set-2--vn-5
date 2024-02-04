e6cd147ad22e36eedc4ef307ded544abd17d34e6
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-02-04 17:38:23 +0100

    kommenterat

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-string[] leksaker = new string[] { "boll", "docka", "lego", "kloss", "bil" };
-string[] klasskamrater = new string[] { "yoel", "adam", "theo", " antoni", "arda" };
-int[] betyg = new int[] { 1, 2, 3, 4, 5 };
+string[] leksaker = new string[] { "boll", "docka", "lego", "kloss", "bil" }; // string-array för leksaker
+string[] klasskamrater = new string[] { "yoel", "adam", "theo", " antoni", "arda" }; // string-array flr leksaker
+int[] betyg = new int[] { 1, 2, 3, 4, 5 }; // integer-array för nummer betygen
 
-for (int i = 0; i < klasskamrater.Length; i++)
+for (int i = 0; i < klasskamrater.Length; i++) // for loopen för att skriva ut vad för betyg de olika klasskamraterna ger leksakerna
 {
     Console.WriteLine(klasskamrater[i] + "ger" + leksaker[i] + "betyget" + betyg[i]);
 }
