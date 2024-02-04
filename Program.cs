c889c2612e7377c900293d5a127729e7e7349667
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-02-04 17:36:57 +0100

    gjort klart for loop med ett problem kunde inte skaffa mellanrum mellan orden

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+string[] leksaker = new string[] { "boll", "docka", "lego", "kloss", "bil" };
+string[] klasskamrater = new string[] { "yoel", "adam", "theo", " antoni", "arda" };
+int[] betyg = new int[] { 1, 2, 3, 4, 5 };
+
+for (int i = 0; i < klasskamrater.Length; i++)
+{
+    Console.WriteLine(klasskamrater[i] + "ger" + leksaker[i] + "betyget" + betyg[i]);
+}
